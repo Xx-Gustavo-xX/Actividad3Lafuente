1ddbabddcdc4cb955d6577fd93c419c862f928cc
Thiago <49259387@fatimarem.edu.ar>
2026-03-18 13:11:28 -0300

    subo actividad 3 lafuente

---
+++ src/Actividad3_Lafuente/personaLafuente.java
@@ -0,0 +1,57 @@
+package Actividad3_Lafuente;
+
+public class personaLafuente {
+
+	String nombre;
+	String apellido;
+	String numeroDocumentoIdentidad;
+	int edad = 0;
+	boolean casado;
+	
+	public void Persona() {
+		
+	}
+
+	
+
+	
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getNumeroDocumentoIdentidad() {
+		return numeroDocumentoIdentidad;
+	}
+	public void setNumeroDocumentoIdentidad(String numeroDocumentoIdentidad) {
+		this.numeroDocumentoIdentidad = numeroDocumentoIdentidad;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public boolean isCasado() {
+		return casado;
+	}
+	public void setCasado(boolean casado) {
+		this.casado = casado;
+	}
+	
+	
+
+	
+	
+	
+}
